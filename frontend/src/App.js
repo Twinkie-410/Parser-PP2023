ae47ce01b784ff3ca47cc97c846aba6d7ec564e7
Daniil Ryzhenko <ryzhenko62@gmail.com>
2023-12-19 16:56:02 +0500

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className="text-center">
+        <span className='text-red-500'>pivo </span>
+        ghgh
+      </header>
+    </div>
+  );
+}
+
+export default App;
